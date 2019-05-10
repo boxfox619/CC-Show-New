9214c79ac31f2a888f64075cf914aa82ea7d93dd
Boxfox <boxfoxsg619@gmail.com>
2019-05-10 15:34:33 +0900

    set image width height 100%

--- src/routes/EditorPage/components/asset/ImageAsset.tsx
+++ src/routes/EditorPage/components/asset/ImageAsset.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface OwnProps {
     value: string
@@ -6,10 +7,15 @@ interface OwnProps {
 
 type Props = OwnProps & React.HTMLAttributes<HTMLImageElement>;
 
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+`
+
 export const ImageAsset: React.FC<Props> = ({value, ...imgProps}) => {
     const onDragStart = (e: React.MouseEvent) => e.preventDefault();
     return (
-        <img {...imgProps}
+        <Image {...imgProps}
             onDragStart={onDragStart}
             src={value}
         />
